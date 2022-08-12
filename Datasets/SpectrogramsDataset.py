Fixed gradient issues

Datasets/SpectrogramsDataset.py
@@ -14,18 +14,23 @@ class SpectrogramsDataset(Dataset):
     def __init__(self, dataset_directory, train, device="cpu", colormap="parula_norm_lab", augmentor=None):
         files = get_files(dataset_directory)
         self.dataset_directory = dataset_directory
-        self.device = device
 
         # Filter spectrograms from files
-        self.spectrograms_L_channel = list(filter(lambda k: f"spectrogram_L_channel_{colormap}" in k, files))  
-        self.labels = list(filter(lambda k: f"labels_{colormap}" in k, files))
-
+        self.spectrograms_L_channel = sorted(list(filter(lambda k: f"spectrogram_L_channel_{colormap}" in k and k.endswith(".zip"), files)), key = lambda filename: int(filename.replace(".", "_").split("_")[-2]))
+        self.labels = sorted(list(filter(lambda k: f"labels_{colormap}" in k and k.endswith(".zip"), files)), key = lambda filename: int(filename.replace(".", "_").split("_")[-2]))         
+                             
         if train:
             dataset_range = slice(len(self.spectrograms_L_channel) // 10, len(self.spectrograms_L_channel))
+            labels_range = slice(len(self.labels) // 10, len(self.labels))
         else:
             dataset_range = slice(len(self.spectrograms_L_channel) // 10)
+            labels_range = slice(len(self.labels) // 10)
 
         self.spectrograms_L_channel = self.spectrograms_L_channel[dataset_range]
+        self.labels = self.labels[dataset_range]
+                             
+        print(self.spectrograms_L_channel[0:10]) 
+        print(self.labels[0:10]) 
 
         # Augmentation
         self.augmentor = augmentor
@@ -33,8 +38,8 @@ class SpectrogramsDataset(Dataset):
     def __getitem__(self, index):  
 
         # Load tensors
-        L_channel_tensor = self.__load_tensor(self.spectrograms_L_channel[index])
-        labels_tensor = self.__load_tensor(self.labels[index])
+        L_channel_tensor = self.__load_tensor(os.path.join(self.dataset_directory , self.spectrograms_L_channel[index]))
+        labels_tensor = self.__load_tensor(os.path.join(self.dataset_directory , self.labels[index]))
         
         # Augment tensor
         if self.augmentor is not None:
@@ -44,13 +49,18 @@ class SpectrogramsDataset(Dataset):
         return L_channel_tensor, labels_tensor
 
     def __load_tensor(self, zip_path):
+        tensor_path = zip_path.replace(".zip", ".pt")
+        if os.path.exists(tensor_path):
+            os.remove(tensor_path)
+                             
         with zipfile.ZipFile(zip_path, 'r') as zip_ref:
             zip_ref.extractall(self.dataset_directory)
 
-        tensor_path = zip_path.replace(".zip", ".pt")
-        return torch.load(tensor_path, map_location=self.device)
+        tensor = torch.load(tensor_path)
+                             
+        os.remove(tensor_path)       
+        return tensor
+        
 
     def __len__(self):
         return len(self.spectrograms_L_channel)
-
-