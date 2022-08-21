2798e9fcf9ca5aafdb001359af72f75409885ec1
Sławomir Nikiel <slawek.nikiel@gmail.com>
2022-08-21 17:12:21 +0000

    Added wandb sweep support

--- train_unet.py
+++ train_unet.py
@@ -7,6 +7,7 @@ from Datasets.SpectrogramsDataset import SpectrogramsDataset
 import torch.nn.functional as torch_func
 import Configuration
 from Models.UNET.unet_model import UNet
+from Models.eccv16 import eccv16
 import Logger
 import munch
 
@@ -49,12 +50,12 @@ def train_one_epoch(model, training_loader, optimizer, config, epoch, step):
         avg_metrics.accuracy += metrics.accuracy
 
         # Report
-        if i % config.batch_checkpoint == (config.batch_checkpoint - 1):
+        if i % global_config.batch_checkpoint == (global_config.batch_checkpoint - 1):
             step +=1
 
             # Calculate current checkpoint metrics
-            current_loss = running_metrics.loss / config.batch_checkpoint
-            current_accuracy = running_metrics.accuracy / config.batch_checkpoint
+            current_loss = running_metrics.loss / global_config.batch_checkpoint
+            current_accuracy = running_metrics.accuracy / global_config.batch_checkpoint
 
             # Log to stdout
             logger.info(f"      batch {i + 1} loss: {current_loss} accuracy: {current_accuracy * 100} %")
@@ -68,8 +69,7 @@ def train_one_epoch(model, training_loader, optimizer, config, epoch, step):
         if (i + 1) == len(training_loader):
             avg_metrics.loss = avg_metrics.loss / len(training_loader)
             avg_metrics.accuracy = avg_metrics.accuracy / len(training_loader)
-
-
+            
     return avg_metrics, step
 
 def validate(model, validation_loader):
@@ -94,57 +94,73 @@ def validate(model, validation_loader):
 
     return avg_metrics
 
-def train(config):
-    # Create datasets for training & validation
-    logger.info("Import training set.")
-    training_set = SpectrogramsDataset(config.spectrogram_files_directory, train=True, size=config.parameters.dataset_size)
-    logger.info("Import validation set.")
-    validation_set = SpectrogramsDataset(config.spectrogram_files_directory, train=False, size=config.parameters.dataset_size)
+def train(config=None):
+    with wandb.init(project="mel-steg-cINN", entity="snikiel", config=config):
+        config = wandb.config    
+    
+        # Create datasets for training & validation
+        logger.info("Import training set.")
+        training_set = SpectrogramsDataset(global_config.spectrogram_files_directory, train=True, size=global_config.dataset_size)
+        logger.info("Import validation set.")
+        validation_set = SpectrogramsDataset(global_config.spectrogram_files_directory, train=False, size=global_config.dataset_size)
 
-    # Create data loaders for our datasets; shuffle for training, not for validation
-    training_loader = torch.utils.data.DataLoader(training_set, batch_size=config.parameters.batch_size, shuffle=True, num_workers=2)
-    validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=config.parameters.batch_size, shuffle=False, num_workers=2)
+        # Create data loaders for our datasets; shuffle for training, not for validation
+        training_loader = torch.utils.data.DataLoader(training_set, batch_size=config.batch_size, shuffle=True, num_workers=2)
+        validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=config.batch_size, shuffle=False, num_workers=2)
 
-    # Create model
-    model = UNet(n_channels=1, n_classes=256).to(device).float()
+        # Create model
+        model = UNet(n_channels=1, n_classes=256)
+        model = model.to(device).float()
 
-    # Optimizers specified in the torch.optim package
-    optimizer = torch.optim.Adam(model.parameters(), lr=config.parameters.learning_rate)
+        # Optimizers specified in the torch.optim package
+        optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
+        
+        # LR scheduler
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', verbose=True, patience=max(2, config.epochs // 10), min_lr=1e-5, factor=0.5)
+        
+        # Define loss function
+        global loss_function
+        loss_function = get_loss_function(config.loss_function).to(device)
 
-    step = 0
-    epoch_durations = []
-    for epoch in range(config.parameters.epochs):
-        logger.info('EPOCH {}:'.format(epoch + 1))
-        epoch_start_time = time.time()
+        step = 0
+        epoch_durations = []
+        for epoch in range(config.epochs):
+            logger.info('EPOCH {}:'.format(epoch + 1))
+            epoch_start_time = time.time()
 
-        # Make sure gradient tracking is on, and do a pass over the data
-        model.train(True)
-        logger.info("       Model training.")
-        train_metrics, step = train_one_epoch(model, training_loader, optimizer, config, epoch, step)
+            # Make sure gradient tracking is on, and do a pass over the data
+            model.train(True)
+            logger.info("       Model training.")
+            train_metrics, step = train_one_epoch(model, training_loader, optimizer, config, epoch, step)
 
-        # We don't need gradients on to do reporting
-        model.train(False)
+            # We don't need gradients on to do reporting
+            model.train(False)
 
-        # Validate model
-        logger.info("       Model validation.")
-        validation_metrics = validate(model, validation_loader)
+            # Validate model
+            logger.info("       Model validation.")
+            validation_metrics = validate(model, validation_loader)
+            
+            # LR schedule
+            if config.enable_lr_scheduler:
+                scheduler.step(train_metrics.loss)
+            wandb.log({"learning_rate": scheduler.optimizer.param_groups[0]['lr']}, step=step)
 
-        # Log to Weights & Biases
-        wandb.log({"avg_train_loss": train_metrics.loss, "avg_train_acc": train_metrics.accuracy}, step=step)
-        wandb.log({"avg_val_loss": validation_metrics.loss, "avg_val_acc": validation_metrics.accuracy}, step=step)
+            # Log to Weights & Biases
+            wandb.log({"avg_train_loss": train_metrics.loss, "avg_train_acc": train_metrics.accuracy}, step=step)
+            wandb.log({"avg_val_loss": validation_metrics.loss, "avg_val_acc": validation_metrics.accuracy}, step=step)
 
-        # Print epoch statistics
-        logger.info(f"      AVG_LOSS train {train_metrics.loss} valid {validation_metrics.loss}")
-        logger.info(f"      AVG_ACCURACY train {train_metrics.accuracy * 100} % valid {validation_metrics.accuracy * 100} %")
+            # Print epoch statistics
+            logger.info(f"      AVG_LOSS train {train_metrics.loss} valid {validation_metrics.loss}")
+            logger.info(f"      AVG_ACCURACY train {train_metrics.accuracy * 100} % valid {validation_metrics.accuracy * 100} %")
 
-        # Log epoch duration
-        epoch_duration = time.time() - epoch_start_time
-        wandb.log({"epoch_runtime (seconds)": epoch_duration}, step=step)
-        epoch_durations.append(epoch_duration)
+            # Log epoch duration
+            epoch_duration = time.time() - epoch_start_time
+            wandb.log({"epoch_runtime (seconds)": epoch_duration}, step=step)
+            epoch_durations.append(epoch_duration)
 
-    # Log average epoch duration
-    avg_epoch_runtime = sum(epoch_durations) / len(epoch_durations)
-    wandb.log({"avg epoch runtime (seconds)": avg_epoch_runtime})
+        # Log average epoch duration
+        avg_epoch_runtime = sum(epoch_durations) / len(epoch_durations)
+        wandb.log({"avg epoch runtime (seconds)": avg_epoch_runtime})
 
 def accuracy(outputs, targets):
     threshold = 0.5    
@@ -192,6 +208,15 @@ def test_CUDA():
         logger.warning("PyTorch is not running on CUDA!")
         return False
     
+def get_loss_function(loss_function_name):
+    loss_func = None
+    if loss_function_name == "DiceLoss":
+        loss_func = DiceLoss()
+    if loss_function_name == "DiceBCELoss":
+        loss_func = DiceBCELoss()
+    if loss_function_name == "IoULoss":
+        loss_func = IoULoss()
+    return loss_func
     
 class DiceLoss(torch.nn.Module):
     def __init__(self, weight=None, size_average=True):
@@ -231,7 +256,29 @@ class DiceBCELoss(torch.nn.Module):
         Dice_BCE = BCE + dice_loss
         
         return Dice_BCE
+    
+class IoULoss(torch.nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(IoULoss, self).__init__()
 
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        #inputs = F.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #intersection is equivalent to True Positive count
+        #union is the mutually inclusive area of all labels & predictions 
+        intersection = (inputs * targets).sum()
+        total = (inputs + targets).sum()
+        union = total - intersection 
+        
+        IoU = (intersection + smooth)/(union + smooth)
+                
+        return 1 - IoU
 
 if __name__ == "__main__":
     # Get logger
@@ -242,8 +289,6 @@ if __name__ == "__main__":
 
     # Initialize Weights & Biases
     wandb.login(key='***REMOVED***')
-    wandb.init(project="mel-steg-cINN", entity="snikiel")
-    wandb.config = config.unet_training.parameters
     
     is_cuda = test_CUDA()
     
@@ -251,8 +296,9 @@ if __name__ == "__main__":
         device = torch.device('cuda')
     else:
         device = torch.device('cpu')
-        
-    loss_function = DiceBCELoss().to(device)
+    
     accuracy_fuction = accuracy
-
-    train(config.unet_training)
+    global_config = config.unet_training.global_parameters
+    
+    sweep_id = wandb.sweep(config.unet_training.sweep_config, project="mel-steg-cINN")
+    wandb.agent(sweep_id, function=train, count=10)
