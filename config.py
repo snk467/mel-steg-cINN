bb95e1fa89b472b5accd6017d3ba93c19c0b1d6b
Sławomir Nikiel <slawek.nikiel@gmail.com>
2022-12-30 12:39:21 +0100

    Bus fixes and improvements

--- config.py
+++ config.py
@@ -82,7 +82,7 @@ class Config(Structure):
     class steg_cinn_training(Structure):   
         clamping = 1.5                      # Clamping parameter in the coupling blocks (higher = less stable but more expressive)
         alpha = 0.1
-
+        rewrite_models = True
         # Training hyperparameters: 
         lr = 5e-6
         lr_feature_net = 1e-4
